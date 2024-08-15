9b61b4a342ec3aac39a3924a849e8b210d98c1c3
sergioserbluk <sergiodserbluk@gmail.com>
2024-08-15 09:16:19 -0300

    Agregar archivos de proyecto.

---
+++ trabajoconarchivos/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TreeViewArchivos = New System.Windows.Forms.TreeView()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.CopiarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PegarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EliminarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TreeViewArchivos
+        '
+        Me.TreeViewArchivos.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.TreeViewArchivos.Location = New System.Drawing.Point(27, 78)
+        Me.TreeViewArchivos.Name = "TreeViewArchivos"
+        Me.TreeViewArchivos.Size = New System.Drawing.Size(511, 283)
+        Me.TreeViewArchivos.TabIndex = 0
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopiarToolStripMenuItem, Me.PegarToolStripMenuItem, Me.EliminarToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(118, 70)
+        '
+        'CopiarToolStripMenuItem
+        '
+        Me.CopiarToolStripMenuItem.Name = "CopiarToolStripMenuItem"
+        Me.CopiarToolStripMenuItem.Size = New System.Drawing.Size(117, 22)
+        Me.CopiarToolStripMenuItem.Text = "copiar"
+        '
+        'PegarToolStripMenuItem
+        '
+        Me.PegarToolStripMenuItem.Name = "PegarToolStripMenuItem"
+        Me.PegarToolStripMenuItem.Size = New System.Drawing.Size(117, 22)
+        Me.PegarToolStripMenuItem.Text = "mover"
+        '
+        'EliminarToolStripMenuItem
+        '
+        Me.EliminarToolStripMenuItem.Name = "EliminarToolStripMenuItem"
+        Me.EliminarToolStripMenuItem.Size = New System.Drawing.Size(117, 22)
+        Me.EliminarToolStripMenuItem.Text = "eliminar"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(219, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(117, 45)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Cargar tree view"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(233, 387)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(76, 37)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Salir"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(565, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TreeViewArchivos)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TreeViewArchivos As TreeView
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents CopiarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PegarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EliminarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ trabajoconarchivos/Form1.vb
@@ -0,0 +1,150 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub TreeViewArchivos_BeforeExpand(sender As Object, e As TreeViewCancelEventArgs) Handles TreeViewArchivos.BeforeExpand
+        'este evento se dispara antes de que un nodo se expanda, por lo que podemos cargar los subdirectorios en este momento
+        Dim expandingNode As TreeNode = e.Node
+
+        If expandingNode.Nodes.Count = 1 AndAlso expandingNode.Nodes(0).Text = "Loading..." Then
+            expandingNode.Nodes.Clear()
+            CargarSubDirectorios(expandingNode)
+        End If
+    End Sub
+
+    Private Sub CargarSubDirectorios(parentNode As TreeNode)
+        Dim dirInfo As DirectoryInfo = CType(parentNode.Tag, DirectoryInfo)
+
+        Try
+            ' Cargar subdirectorios
+            For Each subDir As DirectoryInfo In dirInfo.GetDirectories()
+                Dim dirNode As New TreeNode(subDir.Name)
+                dirNode.Tag = subDir
+                dirNode.Nodes.Add("Loading...") ' Añade un nodo temporal para permitir la expansión 
+                parentNode.Nodes.Add(dirNode)
+            Next
+            ' Cargar archivos
+            For Each file As FileInfo In dirInfo.GetFiles()
+                Dim fileNode As New TreeNode(file.Name)
+                fileNode.Tag = file
+                parentNode.Nodes.Add(fileNode)
+            Next
+        Catch ex As UnauthorizedAccessException
+            MessageBox.Show("Acceso denegado a " & dirInfo.FullName)
+        End Try
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TreeViewArchivos.Nodes.Clear()
+
+        For Each drive As DriveInfo In DriveInfo.GetDrives()
+            If drive.IsReady Then
+                Dim rootNode As New TreeNode(drive.Name)
+                rootNode.Tag = drive.RootDirectory
+                TreeViewArchivos.Nodes.Add(rootNode)
+                CargarSubDirectorios(rootNode)
+            End If
+        Next
+    End Sub
+
+    Private Sub TreeViewArchivos_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeViewArchivos.AfterSelect
+
+    End Sub
+
+    Private Sub TreeViewArchivos_NodeMouseClick(sender As Object, e As TreeNodeMouseClickEventArgs) Handles TreeViewArchivos.NodeMouseClick
+        If e.Button = MouseButtons.Right Then
+            TreeViewArchivos.SelectedNode = e.Node
+        End If
+    End Sub
+
+    Private Sub CopiarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopiarToolStripMenuItem.Click
+        Dim selectedNode As TreeNode = TreeViewArchivos.SelectedNode
+        If selectedNode IsNot Nothing AndAlso TypeOf selectedNode.Tag Is FileInfo Then
+            Dim fileInfo As FileInfo = CType(selectedNode.Tag, FileInfo)
+            Dim saveFileDialog As New SaveFileDialog()
+            saveFileDialog.FileName = fileInfo.Name
+            If saveFileDialog.ShowDialog() = DialogResult.OK Then
+                fileInfo.CopyTo(saveFileDialog.FileName, True)
+                MessageBox.Show("Archivo copiado a " & saveFileDialog.FileName)
+            End If
+        ElseIf selectedNode IsNot Nothing AndAlso TypeOf selectedNode.Tag Is DirectoryInfo Then
+            Dim dirInfo As DirectoryInfo = CType(selectedNode.Tag, DirectoryInfo)
+            Dim folderBrowserDialog As New FolderBrowserDialog()
+            If folderBrowserDialog.ShowDialog() = DialogResult.OK Then
+                Dim destino As String = Path.Combine(folderBrowserDialog.SelectedPath, dirInfo.Name)
+                DirectoryCopy(dirInfo.FullName, destino, True)
+                MessageBox.Show("Directorio copiado a " & destino)
+            End If
+        End If
+    End Sub
+
+    ' Método auxiliar para copiar directorios
+    Private Sub DirectoryCopy(sourceDir As String, destDir As String, copySubDirs As Boolean)
+        Dim dir As DirectoryInfo = New DirectoryInfo(sourceDir)
+        Dim dirs As DirectoryInfo() = dir.GetDirectories()
+
+        If Not Directory.Exists(destDir) Then
+            Directory.CreateDirectory(destDir)
+        End If
+
+        Dim files As FileInfo() = dir.GetFiles()
+        For Each file As FileInfo In files
+            Dim tempPath As String = Path.Combine(destDir, file.Name)
+            file.CopyTo(tempPath, False)
+        Next
+
+        If copySubDirs Then
+            For Each subDir As DirectoryInfo In dirs
+                Dim tempPath As String = Path.Combine(destDir, subDir.Name)
+                DirectoryCopy(subDir.FullName, tempPath, copySubDirs)
+            Next
+        End If
+    End Sub
+
+    Private Sub PegarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PegarToolStripMenuItem.Click
+        Dim selectedNode As TreeNode = TreeViewArchivos.SelectedNode
+        If selectedNode IsNot Nothing AndAlso TypeOf selectedNode.Tag Is FileInfo Then
+            Dim fileInfo As FileInfo = CType(selectedNode.Tag, FileInfo)
+            Dim folderBrowserDialog As New FolderBrowserDialog()
+            If folderBrowserDialog.ShowDialog() = DialogResult.OK Then
+                Dim destino As String = Path.Combine(folderBrowserDialog.SelectedPath, fileInfo.Name)
+                fileInfo.MoveTo(destino)
+                MessageBox.Show("Archivo movido a " & destino)
+                selectedNode.Remove()
+            End If
+        ElseIf selectedNode IsNot Nothing AndAlso TypeOf selectedNode.Tag Is DirectoryInfo Then
+            Dim dirInfo As DirectoryInfo = CType(selectedNode.Tag, DirectoryInfo)
+            Dim folderBrowserDialog As New FolderBrowserDialog()
+            If folderBrowserDialog.ShowDialog() = DialogResult.OK Then
+                Dim destino As String = Path.Combine(folderBrowserDialog.SelectedPath, dirInfo.Name)
+                dirInfo.MoveTo(destino)
+                MessageBox.Show("Directorio movido a " & destino)
+                selectedNode.Remove()
+            End If
+        End If
+    End Sub
+
+    Private Sub EliminarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EliminarToolStripMenuItem.Click
+        Dim selectedNode As TreeNode = TreeViewArchivos.SelectedNode
+        If selectedNode IsNot Nothing AndAlso TypeOf selectedNode.Tag Is FileInfo Then
+            Dim fileInfo As FileInfo = CType(selectedNode.Tag, FileInfo)
+            If MessageBox.Show("¿Está seguro de eliminar este archivo?", "Confirmación", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+                fileInfo.Delete()
+                MessageBox.Show("Archivo eliminado")
+                selectedNode.Remove()
+            End If
+        ElseIf selectedNode IsNot Nothing AndAlso TypeOf selectedNode.Tag Is DirectoryInfo Then
+            Dim dirInfo As DirectoryInfo = CType(selectedNode.Tag, DirectoryInfo)
+            If MessageBox.Show("¿Está seguro de eliminar este directorio?", "Confirmación", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+                dirInfo.Delete(True)
+                MessageBox.Show("Directorio eliminado")
+                selectedNode.Remove()
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
